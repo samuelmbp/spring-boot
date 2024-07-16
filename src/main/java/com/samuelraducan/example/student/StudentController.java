37e1f179a42dba0802ec15f9f0db495a332ca6ee
Samuel Raducan <samuel.thembp@gmail.com>
2024-07-16 09:15:06 +0100

    Add fields validation for first and last name in StudentDto

--- src/main/java/com/samuelraducan/example/student/StudentController.java
+++ src/main/java/com/samuelraducan/example/student/StudentController.java
@@ -1,9 +1,15 @@
 package com.samuelraducan.example.student;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StudentController {
@@ -20,7 +26,9 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public StudentResponseDto saveStudent(@RequestBody StudentDto studentDto) {
+    public StudentResponseDto saveStudent(
+            @Valid @RequestBody StudentDto studentDto
+    ) {
         return studentService.createStudent(studentDto);
     }
 
@@ -43,5 +51,21 @@ public class StudentController {
     @ResponseStatus(HttpStatus.OK)
     public void deleteStudent(@PathVariable("id") Integer id) {
         studentService.deleteStudentById(id);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult()
+                .getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/samuelraducan/example/student/StudentDto.java
+++ src/main/java/com/samuelraducan/example/student/StudentDto.java
@@ -1,8 +1,14 @@
 package com.samuelraducan.example.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+        @NotEmpty(message = "First name should not be empty")
         String firstName,
+
+        @NotEmpty(message = "Last name should not be empty")
         String lastName,
+
         String email,
         Integer schoolId
 ) {
