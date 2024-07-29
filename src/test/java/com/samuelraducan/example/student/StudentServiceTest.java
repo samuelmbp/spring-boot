08824daebe01c3711df48c00ec8a8014729c958f
Samuel Raducan <samuel.thembp@gmail.com>
2024-07-29 09:40:43 +0100

    Added unit tests to find all students

--- src/test/java/com/samuelraducan/example/student/StudentServiceTest.java
+++ src/test/java/com/samuelraducan/example/student/StudentServiceTest.java
@@ -6,6 +6,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -59,5 +63,26 @@ class StudentServiceTest {
                 .save(student);
         verify(studentMapper, times(1))
                 .toStudentResponseDto(savedStudent);
+    }
+
+    @Test
+    public void should_return_all_students() {
+        // Given
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("John", "Doe", "john@mail.com", 1));
+
+        // Mock the calls
+        when(studentRepository.findAll()).thenReturn(students);
+        when(studentMapper.toStudentResponseDto(any(Student.class)))
+                .thenReturn(new StudentResponseDto(
+                        "John", "Doe", "john@mail.com"
+                ));
+
+        List<StudentResponseDto> studentResponseDtos =
+                studentService.findAllStudents();
+
+        // When
+        assertEquals(students.size(), studentResponseDtos.size());
+        verify(studentRepository, times(1)).findAll();
     }
 }
